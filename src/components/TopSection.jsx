666b02a737710b965c4beaae4c1c36b4ac1c01c2
safiur799 <safiurrahaman799@gmail.com>
2021-09-13 14:41:36 +0530

    second20MinCommit

---
+++ src/components/TopSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TopSection() {
+  return (
+    <div>
+      <h1 className="text-center">Grocery Management Web App</h1>
+      <hr />
+
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    </div>
+  );
+}
