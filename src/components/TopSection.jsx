430fddc1645aa451038655e27107c1081a05ece2
safiur799 <safiurrahaman799@gmail.com>
2021-09-13 15:02:42 +0530

    third20min

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-
+import { useState } from "react";
 export default function TopSection() {
+  const [grocery, setGrocery] = useState([]);
+  const changeHandler = (e) => {
+    setGrocery(e.target.value);
+  };
   return (
     <div>
       <h1 className="text-center">Grocery Management Web App</h1>
       <hr />
 
       <div className="container">
-        <div className="row"></div>
+        <div className="d-flex justify-content-around">
+          <input
+            type="text"
+            name="grocery"
+            placeholder="enter grocery"
+            className="text-center"
+            value={grocery}
+            onChange={changeHandler}
+          />
+          <button className="btn btn-primary">Add To FinalList</button>
+          <button className="btn btn-primary">Add To WishList</button>
+        </div>
       </div>
     </div>
   );
